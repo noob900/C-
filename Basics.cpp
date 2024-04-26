37fa094904485029782611c63d6de3a6ebf8da9b
noob900 <meshishir12@gmail.com>
2024-04-26 16:18:58 +0300

    charmessage

--- Basics.cpp
+++ Basics.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main(){
 
-int number, revnumber 
+int height, width;
 
+cout<<"height";cin>>height;
+cout<<"width";cin>>width;
 
+for (int i = 0; i < height; i++){
 
+  for (int j = 0; j < width; j++){
+    cout<<"*";
+  }
+  cout<<endl;
+}
 
 } 

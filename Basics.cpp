2a9c1a789e5782b221367f862d09df37fd71c493
noob900 <62810767+noob900@users.noreply.github.com>
2024-04-26 14:39:01 +0300

    Update Basics.cpp

--- Basics.cpp
+++ Basics.cpp
@@ -5,13 +5,13 @@ int main()
 {
 
 int number, revnumber=0; 
-cout<<"enter atleast 2 digit number:"; cin >> number; //123
+cout<<"enter atleast 2 digit number:"; cin >> number; 
 
 do
 {
 cout<<number<<endl;
-revnumber=(number%10+revnumber*10); // 32
-number=number/10; // 1
+revnumber=(number%10+revnumber*10); 
+number=number/10; 
  
 } while(number!=0);
 

142591d55cd012cafa1e16354ce7795a9a00fc14
noob900 <meshishir12@gmail.com>
2024-04-26 13:54:05 +0300

    first v

--- Basics.cpp
+++ Basics.cpp
@@ -4,9 +4,13 @@ using namespace std;
 int main()
 {
 
-int number, revnumber 
+int number, revnumber; 
+cout<<"enter atleast 2 digit number:"; cin >> number;
 
+revnumber=number%10;
+number=number/10; 
 
-
+cout<<number;
+cout<<revnumber;
 
 } 

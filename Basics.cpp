db1986d42c5b0576a5be04215b906068b21dcbb0
noob900 <meshishir12@gmail.com>
2024-04-27 12:10:09 +0300

    rectangle

--- Basics.cpp
+++ Basics.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main(){
 
-int height, width;
+int height, width, space;
+char symbol;
 
-cout<<"height";cin>>height;
-cout<<"width";cin>>width;
+cout<<"height:";cin>>height;
+cout<<"width:";cin>>width;
+cout<<"symbol:";cin>>symbol;
+cout<<"spacing:";cin>>space;
 
 for (int i = 0; i < height; i++){
 
   for (int j = 0; j < width; j++){
-    cout<<"*";
+    cout<<setw(space)<<symbol;
   }
   cout<<endl;
 }

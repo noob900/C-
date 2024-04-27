ee726fc9d0187a6764ffb63b996a0e9c49b1849d
noob900 <meshishir12@gmail.com>
2024-04-27 14:04:07 +0300

    big triangle

--- Basics.cpp
+++ Basics.cpp
@@ -19,4 +19,10 @@ for (int i = 0; i <= height; i++){
   cout<<endl;
 }
 
-} 
+for (int i = width; i >= 1; i--){
+  for (int j = 1; j<=i; j++){
+    cout<<setw(space)<<symbol;
+  }
+  cout<<endl;
+}
+}

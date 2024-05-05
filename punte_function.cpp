ff5b42081b02b0d21f733da394f22f0d0629643b
noob900 <meshishir12@gmail.com>
2024-05-05 16:58:18 +0300

    list <string>

--- punte_function.cpp
+++ punte_function.cpp
@@ -15,6 +15,7 @@ class persondetails{
         int Age; 
         string Address; 
         string School; 
+        list<string> Courses; 
 
      //constructor
     persondetails(std::string name,string address, string school){
@@ -22,15 +23,18 @@ class persondetails{
         Age=0;
         Address = address;
         School=school;
+
+        cout<< "Full name of given person is " << Fullname<<endl; 
+        cout<<Fullname<< " address is " << Address<<endl; 
+        cout << Fullname << " studies in " << School<<endl;
     }
 
-
-    
 };
 
 int main(){
-    persondetails shishir("shishir","lappeenranta", "LUT");
+    persondetails shishir("shishir pathak","lappeenranta", "LUT");
+    shishir.Courses.push_back("Contorls");
+    shishir.Courses.push_back("Machine Dynamics");
 
-    cout<< "Full name of given person is:" << shishir.Fullname<<endl; 
-
+    persondetails Nima("Nima Palmu Sherpa","lappeenranta", "LAB");
 }

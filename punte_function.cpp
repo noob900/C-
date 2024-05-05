8bbf2eccf6358485ee36693940293413018e9eb5
noob900 <meshishir12@gmail.com>
2024-05-05 17:27:00 +0300

    constructor and method

--- punte_function.cpp
+++ punte_function.cpp
@@ -24,9 +24,17 @@ class persondetails{
         Address = address;
         School=school;
 
+    }
+
+    void get_info(){
         cout<< "Full name of given person is " << Fullname<<endl; 
         cout<<Fullname<< " address is " << Address<<endl; 
         cout << Fullname << " studies in " << School<<endl;
+        for (string Courses: Courses){
+       cout<<Courses<<endl;
+    };
+
+
     }
 
 };
@@ -35,6 +43,8 @@ int main(){
     persondetails shishir("shishir pathak","lappeenranta", "LUT");
     shishir.Courses.push_back("Contorls");
     shishir.Courses.push_back("Machine Dynamics");
+
+    shishir.get_info();
 
     persondetails Nima("Nima Palmu Sherpa","lappeenranta", "LAB");
 }

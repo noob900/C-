0ffcbd712713957d7592db31c0474764ce6fe048
noob900 <meshishir12@gmail.com>
2024-05-06 16:41:35 +0300

    tweaking word challenge

--- punte_function.cpp
+++ punte_function.cpp
@@ -1,21 +1,83 @@
 #include <iostream>
 #include <list>
+#include<vector>
 using namespace std; 
+
+class words{
+
+private:
+    vector<char> internalsavedword;
+    int internalnumberofelement;
+
+public:
+    // this is a constructor
+    words(string inputword){
+
+    //saves the input word in an array. 
+    vector<char> savedword;
+    for(int i = 0; i < inputword.length(); i++){
+        savedword.push_back(inputword[i]);
+    }  
+    // prints the array list  
+    int numberofelement=savedword.size(); 
+    for(const char &savedword : savedword){
+        cout<<savedword;
+    } 
+        
+    cout<<endl<<numberofelement<<endl;
+    internalsavedword=savedword;
+    internalnumberofelement=numberofelement;
+    };
+
+    // this is a Method to reverse the word
+    void reverseword(){
+        vector<char> reversedword;
+        for(int i = internalnumberofelement; i >= 0; i--){
+            reversedword.push_back(internalsavedword[i]);
+            cout<<internalsavedword[i]; 
+        }
+    }
+
+    void orders(vector<int> ordernumber){
+        
+    
+      vector<char> orderletter=internalsavedword;
+      for(int i = 0; i < ordernumber.size(); i++){
+          int asciiValue = static_cast<int>(orderletter[i]);
+          
+          switch(ordernumber[i]){
+                case 1: 
+                    asciiValue=asciiValue+1;
+                    orderletter[i]= static_cast<char>(asciiValue);
+                    cout<<orderletter[i]<<"---"<<asciiValue<<endl;
+                    break; 
+              
+                case -1: 
+                    asciiValue=asciiValue-1;
+                    orderletter[i]= static_cast<char>(asciiValue);
+                    cout<<orderletter[i]<<"---"<<asciiValue<<endl;
+                    break;
+          }
+
+      }  
+  }
+};
+
 
 class persondetails{
     
     //attributes
 
     private:
-        string boyfriend;
-        float salary; 
-
-    public:
         string Fullname;
         int Age; 
         string Address; 
         string School; 
         list<string> Courses; 
+        string boyfriend;
+        float salary; 
+
+    public:
 
      //constructor
     persondetails(std::string name,string address, string school){
@@ -23,7 +85,6 @@ class persondetails{
         Age=0;
         Address = address;
         School=school;
-
     }
 
     void get_info(){
@@ -31,20 +92,31 @@ class persondetails{
         cout<<Fullname<< " address is " << Address<<endl; 
         cout << Fullname << " studies in " << School<<endl;
         for (string Courses: Courses){
-       cout<<Courses<<endl;
-    };
-
-
+            cout<<Courses<<endl;
+        };
     }
 
+    void addcourses(string coursename){
+        Courses.push_back(coursename);
+    }
+
+    void coursesize(){
+         cout<< Courses.size() << endl;
+    }
 };
 
+void printdata(void* ptr, char type){
+    switch(type){
+
+    case 'i': cout << *((int*) ptr);
+    case 'c': cout << *((char*) ptr);  
+    }
+   
+}
+
 int main(){
-    persondetails shishir("shishir pathak","lappeenranta", "LUT");
-    shishir.Courses.push_back("Contorls");
-    shishir.Courses.push_back("Machine Dynamics");
 
-    shishir.get_info();
+    words tweakword("hello"); 
+    tweakword.orders({1,1,1,1,1});
 
-    persondetails Nima("Nima Palmu Sherpa","lappeenranta", "LAB");
 }

08d75390643e531f147000b37ab870dc7e49bd7d
noob900 <meshishir12@gmail.com>
2024-05-12 11:26:17 +0300

    plotting the econoomy

--- scrapper.py
+++ scrapper.py
@@ -1,4 +1,3 @@
-
 from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
 import requests
@@ -13,32 +12,14 @@ table_column_header=[titles.text.strip() for titles in table_column]
 table_column_data=soup.find('table',class_='wikitable sortable').findAll('tr')
 
 df=pd.DataFrame(columns=table_column_header)
-
 for row in table_column_data[1:]: 
-    table_row=row.find_all('td')
-    array_size=len(table_column_data)
-    table_row_data= [titles.text.strip() for titles in table_row]
-    df.loc[len(df)]=table_row_data
-    
-print(df)
-
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+       table_row=row.find_all('td')
+       array_size=len(table_column_data)
+       table_row_data= [titles.text.strip() for titles in table_row]
+       df.loc[len(df)]=table_row_data 
+  
+df.to_csv('economy_of_finland.csv')  
+df=pd.read_csv('economy_of_finland.csv')  
+df.head()
+df.plot(x='Unemployment (in%)',kind='bar')
+plt.show()

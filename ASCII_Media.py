9fd5a68a494c5ba97a84e2c98fc89dfb721bf99d
noob900 <meshishir12@gmail.com>
2026-01-07 23:09:28 +0200

    controls practice programs and  simulation

---
+++ ASCII_Media.py
@@ -0,0 +1,19 @@
+# ASCII_Media.py
+import p5   
+from p5 import *
+import numpy as np
+import cv2
+import os
+import random
+
+def Setup():
+    p5.size(400, 400)
+
+def preload():
+    Me=p5.loadImage("IMG_4813.CR3")
+
+def draw():
+    p5.background(255)
+
+
+
